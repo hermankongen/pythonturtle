a2f62a93194b64ae697c79f0dc1267880eead177
hermankongen <37117219+hermankongen@users.noreply.github.com>
2021-01-06 15:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import turtle 
  
-h =  turtle.Turtle()
+h =  turtle.Turtle() #variabel som gjør sån at jeg ikke må skrive om igen jeg tenetger bare og skrive h.
 
 
-for i in range(50):
-    h.forward(69)
-    h.left(6)
+for i in range(50): #koden repiterer seg eller går i loop 
+    h.forward(69)# får turtle til og bevege seg 
+    h.left(6)# betemer retnigen av hvor turtle skal gå 
     h.forward(90)
     h.forward(100)
     h.left(90)
